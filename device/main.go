bff20c1d26167479a56ec5db5e1b895522ee62d0
Luke Day <puke929@gmail.com>
2023-07-31 10:39:34 +0100

    feat: moving event loop into go routine

--- device/main.go
+++ device/main.go
@@ -18,13 +18,17 @@ func main() {
 		cardReader.read()
 	}()
 
-	for {
-		select {
-		case data := <-cardReader.c:
-			log.Println("UID:", hex.EncodeToString(data))
-		case <-keySignal:
-			log.Println("SIGINT detected, closing toddler-player")
-			return
+	go func() {
+		for {
+			select {
+			case data := <-cardReader.c:
+				log.Println("UID:", hex.EncodeToString(data))
+				// Send HTTP request to server with UID
+			case <-keySignal:
+				log.Println("SIGINT detected, closing toddler-player")
+				os.Exit(1)
+			}
 		}
-	}
+	}()
+
 }
