695658f6936567d34c5f8701df0f9ba225358d86
Luke Day <puke929@gmail.com>
2023-08-01 09:22:02 +0100

    fix: for loop didn't need to be in a go routing

--- device/main.go
+++ device/main.go
@@ -20,17 +20,15 @@ func main() {
 		cardReader.Read()
 	}()
 
-	go func() {
-		for {
-			select {
-			case data := <-cardReader.C:
-				log.Println("UID:", hex.EncodeToString(data))
-				// Send HTTP request to server with UID
-			case <-keySignal:
-				log.Println("SIGINT detected, closing toddler-player")
-				os.Exit(1)
-			}
+	for {
+		select {
+		case data := <-cardReader.C:
+			log.Println("UID:", hex.EncodeToString(data))
+			// Send HTTP request to server with UID
+		case <-keySignal:
+			log.Println("SIGINT detected, closing toddler-player")
+			os.Exit(1)
 		}
-	}()
+	}
 
 }
